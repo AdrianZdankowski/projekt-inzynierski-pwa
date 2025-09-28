3c9638cfe8a9c235785bac3965993ba3a60b61e6
Adrian Zdankowski <adrianzdankowski@gmail.com>
2025-09-28 18:56:51 +0200

    Move axios interceptor wrapper to component folder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,7 +15,7 @@ import UserFilesPageTheme from './themes/pages/UserFilesPageTheme.ts'
 import VideoTestPage from './pages/VideoTestPage.tsx'
 import VideoPlayerTheme from './themes/components/VideoPlayerTheme.ts'
 import AuthTheme from './themes/auth/AuthTheme.ts'
-import AxiosInterceptorWrapper from './lib/AxiosInterceptorWrapper.ts'
+import AxiosInterceptorWrapper from './components/AxiosInterceptorWrapper.ts'
 
 function App() {
   const {isAuthenticated} = useAuth();
