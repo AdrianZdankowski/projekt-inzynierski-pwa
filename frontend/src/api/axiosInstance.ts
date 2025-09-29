77e248f8f6172ea2ada1194fd4b3b05383042c82
arekflis <arkadiuszflisikowski10@gmail.com>
2025-09-29 23:20:30 +0200

    Fix logout issue when backend is disconnected

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import { API_BASE_URL } from "./axiosConfig";
 
 const axiosInstance = axios.create({
-    baseURL: API_BASE_URL
+    baseURL: API_BASE_URL,
+    timeout: 1000,
 });
 
 export default axiosInstance;
