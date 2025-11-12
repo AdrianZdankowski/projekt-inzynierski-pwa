e5a0bfd5f0c2799e4627b8a0471099e44c977a67
Adrian Zdankowski <adrianzdankowski@gmail.com>
2025-11-12 14:04:08 +0100

    Add initial caching implementation

--- frontend/src/sw.ts
+++ frontend/src/sw.ts
@@ -1,6 +1,9 @@
 /// <reference lib="webworker" />
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { CacheFirst, NetworkFirst } from 'workbox-strategies'
+import { ExpirationPlugin } from 'workbox-expiration'
+import { CacheableResponsePlugin } from 'workbox-cacheable-response'
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -15,14 +18,92 @@ precacheAndRoute(self.__WB_MANIFEST)
 // clean old assets
 cleanupOutdatedCaches()
 
-/** @type {RegExp[] | undefined} */
-let allowlist : undefined | RegExp[]
-// in dev mode, we disable precaching to avoid caching issues
-if (import.meta.env.DEV)
-  allowlist = [/^\/$/]
+// cache tras React Router
+const handler = createHandlerBoundToURL('/index.html')
+const navigationRoute = new NavigationRoute(handler, {
+  denylist: [/^\/api\//, /\/assets\//],
+})
 
-// to allow work offline
-registerRoute(new NavigationRoute(
-  createHandlerBoundToURL('index.html'),
-  { allowlist },
-))
+registerRoute(navigationRoute)
+
+// cache dla statycznych plików html/js/css
+registerRoute(
+  ({ request }) => ['script', 'style', 'document'].includes(request.destination),
+  new CacheFirst({
+    cacheName: 'static-assets',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 60 * 60 * 24 * 7, // 7 dni
+      }),
+    ],
+  })
+)
+
+// cache listy plików
+registerRoute(
+  ({ url }) =>
+    url.pathname === '/api/file' ||
+    (url.pathname === '/api/file' && url.search.length > 0),
+  new NetworkFirst({
+    cacheName: 'file-list-cache',
+    networkTimeoutSeconds: 3,
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 20,
+        maxAgeSeconds: 10 * 60, // 10 minut
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+  'GET'
+)
+
+
+// cache pojedynczego pliku
+registerRoute(
+  ({ url }) => /^\/api\/file\/[^/]+$/.test(url.pathname),
+  new CacheFirst({
+    cacheName: 'file-metadata-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 60 * 60 * 24, // 1 dzień
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+  'GET'
+)
+
+// cache SAS URL z Azure Blob Storage
+registerRoute(
+  ({ url, request }) => url.origin.includes('blob.core.windows.net') 
+  && request.destination === 'image',
+  new CacheFirst({
+    cacheName: 'azure-blob-cache',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 20,
+        maxAgeSeconds: 60 * 25, // 25 minut
+      }),
+    ],
+  }),
+  'GET'
+)
+
+// /** @type {RegExp[] | undefined} */
+// let allowlist : undefined | RegExp[]
+// // in dev mode, we disable precaching to avoid caching issues
+// if (import.meta.env.DEV)
+//   allowlist = [/^\/$/]
+
+// // to allow work offline
+// registerRoute(new NavigationRoute(
+//   createHandlerBoundToURL('index.html'),
+//   { allowlist },
+// ))
