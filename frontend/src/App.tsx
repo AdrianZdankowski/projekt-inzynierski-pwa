0d9279bda196b9eab6c1f920a520614aac395502
arekflis <arkadiuszflisikowski10@gmail.com>
2025-11-23 01:04:01 +0100

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import UserFilesPage from './pages/UserFilesPage.tsx'
 import { useAuth } from './context/AuthContext.tsx'
 import VideoTestPage from './pages/VideoTestPage.tsx'
 import AxiosInterceptorWrapper from './components/AxiosInterceptorWrapper.ts'
-import Notification from './components/NotificationContainer.tsx'
+import Notification from './components/Notification.tsx'
 
 function App() {
   const {isAuthenticated} = useAuth();
