0f1a3e334101b2fe13a54d87e71a63071f21fac8
Adrian Zdankowski <adrianzdankowski@gmail.com>
2025-12-04 21:17:12 +0100

    Update video file metadata, add docx and xlsx files

--- backend/PwaApp.Application/Seed/DbSeeder.cs
+++ backend/PwaApp.Application/Seed/DbSeeder.cs
@@ -33,12 +33,12 @@ public static class DbSeeder
 
             var video = new WebApplication1.File
             {
-                id = Guid.Parse("4138879d-1bd0-407a-8d2a-6906a1c20320"),
+                id = Guid.Parse("8762AA99-5D4F-4880-9947-8F40E723EBA5"),
                 UserId = Guid.Parse("0EDDE0A7-4DBE-40B4-7834-08DDFD222C2B"),
                 FileName = "gameplay.mp4",
                 MimeType = "video/mp4",
-                Size = 4294967296,
-                BlobName = "0EDDE0A7-4DBE-40B4-7834-08DDFD222C2B/4138879d-1bd0-407a-8d2a-6906a1c20320/gameplay.mp4",
+                Size = 228283152,
+                BlobName = "0edde0a7-4dbe-40b4-7834-08ddfd222c2b/8762aa99-5d4f-4880-9947-8f40e723eba5/gameplay.mp4",
                 UploadTimestamp = DateTime.Now,
                 Status = FileStatus.Uploaded,
                 Checksum = "16PezbYoDrDvOgWaw16g7g=="
@@ -46,15 +46,15 @@ public static class DbSeeder
             
             var jpeg = new WebApplication1.File
             {
-                id = Guid.Parse("B905F8DF-6731-49E0-B29E-0B0B8B1680AB"),
+                id = Guid.Parse("A53E45AF-8794-41DE-80C8-C7065F8E34DE"),
                 UserId = Guid.Parse("0EDDE0A7-4DBE-40B4-7834-08DDFD222C2B"),
-                FileName = "5rx28e7o82rf1.jpeg",
+                FileName = "budynek-vuitton-nyc-2025.jpg",
                 MimeType = "image/jpeg",
-                Size = 147461,
-                BlobName = "0edde0a7-4dbe-40b4-7834-08ddfd222c2b/b905f8df-6731-49e0-b29e-0b0b8b1680ab/5rx28e7o82rf1.jpeg",
+                Size = 300407,
+                BlobName = "0edde0a7-4dbe-40b4-7834-08ddfd222c2b/a53e45af-8794-41de-80c8-c7065f8e34de/budynek-vuitton-nyc-2025.jpg",
                 UploadTimestamp = DateTime.Now,
                 Status = FileStatus.Uploaded,
-                Checksum = "nZkvhXS/lqidjTACEdeAkw=="
+                Checksum = "En+BXy7UpcCgeWXoS3y+uQ=="
             };
             
             var pdf = new WebApplication1.File
@@ -69,7 +69,33 @@ public static class DbSeeder
                 Status = FileStatus.Uploaded,
                 Checksum = "x9NU/xDIaHUvGwMuWw5V6A=="
             };
-            
+
+            var xlsx = new WebApplication1.File
+            {
+                id = Guid.Parse("D0ED73E5-0321-4C58-8CAB-DBC47E521F34"),
+                UserId = Guid.Parse("0EDDE0A7-4DBE-40B4-7834-08DDFD222C2B"),
+                FileName = "file_example_XLSX_50.xlsx",
+                MimeType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                Size = 7360,
+                BlobName = "0edde0a7-4dbe-40b4-7834-08ddfd222c2b/d0ed73e5-0321-4c58-8cab-dbc47e521f34/file_example_XLSX_50.xlsx",
+                UploadTimestamp = DateTime.Now,
+                Status = FileStatus.Uploaded,
+                Checksum = "XCVvLeVxJsrFrGKc5CynVA=="
+            };
+
+            var docx = new WebApplication1.File
+            {
+                id = Guid.Parse("316C3283-E66A-40C3-8F5B-E4C082BA5CFD"),
+                UserId = Guid.Parse("0EDDE0A7-4DBE-40B4-7834-08DDFD222C2B"),
+                FileName = "word-sample.docx",
+                MimeType = "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                Size = 551820,
+                BlobName = "0edde0a7-4dbe-40b4-7834-08ddfd222c2b/316c3283-e66a-40c3-8f5b-e4c082ba5cfd/word-sample.docx",
+                UploadTimestamp = DateTime.Now,
+                Status = FileStatus.Uploaded,
+                Checksum = "1FBMkbBwTq7wyNCY1NOWbA=="
+            };
+
             var gif = new WebApplication1.File
             {
                 id = Guid.Parse("7905CE21-8A62-4AAA-91C1-01E388F6FB19"),
@@ -88,6 +114,8 @@ public static class DbSeeder
             
             context.Files.Add(video);
             context.Files.Add(jpeg);
+            context.Files.Add(xlsx);
+            context.Files.Add(docx);
             context.Files.Add(pdf);
             context.Files.Add(gif);
             
